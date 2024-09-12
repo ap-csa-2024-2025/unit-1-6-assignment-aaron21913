a20ee441409dcfc3278b6ee75121eab7844012ad
aaron <atngo106@student.hbuhsd.edu>
2024-09-12 11:20:19 -0700

    dab

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,15 @@
+public class Problem2
+{
+  public static void main(String[] args)
+  {
+    double numberOne = 57.3934;
+    double numberTwo = 22.5211;
+    
+    int val1 = (int) (numberOne + 0.5);
+    int val2 = (int) (numberTwo + 0.5);
+
+    int answer = val1 + val2;
+    System.out.println(answer);
+  }
+}
 
--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,14 @@
+public class Problem3
+{
+  public static void main(String[] args)
+  {
+    final double num = 62.1435;
+    int first = (int)((num % 1) * 10);
+    int second = (int)((num % 0.1) * 100);
+    int third = (int)((num % 0.01) * 1000);
+
+    System.out.println("decimal number: " + num);
+    System.out.println("answer: " + first + " " + second + " " + third); 
+ }
+}
 
